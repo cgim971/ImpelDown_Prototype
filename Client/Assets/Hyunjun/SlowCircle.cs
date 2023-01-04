186a67be38c1cd19c4f95445533133261e8a3c8b
HyunjunDev <101493216+HyunjunDev@users.noreply.github.com>
2023-01-04 09:25:27 +0900

    원 느려지는 거 수정

--- Client/Assets/Hyunjun/SlowCircle.cs
+++ Client/Assets/Hyunjun/SlowCircle.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SlowCircle : MonoBehaviour
 {
-    public void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerStay2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
